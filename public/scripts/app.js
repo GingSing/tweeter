9e4c50c0bd24187d04f4656456c1767185c064b1
Gary <garyyue2017@gmail.com>
2019-08-13 23:40:13 +0000

    added tweet generator function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,34 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweetObj) => {
-  let articleElem = $("<article>");
-  articleElem.addClass("tweet");
+$(() => {
+  const createTweetElement = (tweetObj) => {
+    let articleElem = $("<article>");
+    articleElem.addClass("tweet");
 
-  let headerElem = $("<header>");
+    let headerElem = $("<header>");
 
-  let nameLabel = $("<label>");
-  nameLabel
+    let userDiv = $("<div>");
+    userDiv.addClass("user");
 
-  let handlerLabel = $("<handler>");
+    let avatarImg = $("<img>");
+    avatarImg.attr('src', tweetObj.user.avatars);
+    avatarImg.attr('alt', "avatar.png");
 
-  let content = $("<p>");
+    let nameLabel = $("<label>");
+    nameLabel.addClass("name");
+    nameLabel.text(tweetObj.user.name);
 
-  let footer = $("<footer>");
+    userDiv.append(avatarImg);
+    userDiv.append(nameLabel);
 
-  headerElem.append(nameLabel);
-  headerElem.append(handlerLabel);
-  headerElem.append(content);
-  headerElem.append(footer);
-  article.append(headerElem);
+    let handlerLabel = $("<label>");
+    handlerLabel.addClass("handler");
+    handlerLabel.text(tweetObj.user.handle);
 
+    let content = $("<p>");
+    content.text(tweetObj.content.text);
 
-  <article class="tweet">
-            <header>
-              <label class="name">Newton</label>
-              <label class="handler">@SirIsaac</label>
-            </header>
-            <p>Wow! What a tweeterific app!</p>
-            <footer>10 days ago</footer>
-          </article>
-};
+    let footer = $("<footer>");
+    footer.text(`${calculateDaysAgo(tweetObj.created_at)} days ago`)
+
+    headerElem.append(userDiv);
+    headerElem.append(handlerLabel);
+    articleElem.append(headerElem);
+
+    articleElem.append(content);
+    articleElem.append(footer);
+
+    return articleElem;
+  };
+
+  const calculateDaysAgo = (time) => {
+    return new Date(time).getDate() - (new Date).getDate();
+  }
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
